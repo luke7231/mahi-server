d6d00520145f35afe16cbc4db94faed87df19869
luke7231 <tmddnjs7231@gmail.com>
2024-08-06 23:05:40 +0900

    feat: 이미 존재하는 토큰이면 또 만들지 않는다.

--- src/resolvers/token/tokenResolvers.ts
+++ src/resolvers/token/tokenResolvers.ts
@@ -8,8 +8,18 @@ export const tokenResolvers = {
   //     },
   //   },
   Mutation: {
-    createToken: async (_, { data }) => {
-      console.log(data);
+    createToken: async (_, { data }: { data: { token: string } }) => {
+      const existingToken = await prisma.expo_Token.findUnique({
+        where: {
+          token: data.token,
+        },
+      });
+
+      // If it already exists, return it (or handle according to your needs)
+      if (existingToken) {
+        return existingToken;
+      }
+
       const newToken = await prisma.expo_Token.create({
         data,
       });
