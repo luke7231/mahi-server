3387aebc4c254de66508e44b81d1ab9168d3b89b
luke7231 <tmddnjs7231@gmail.com>
2024-08-20 18:06:29 +0900

    feat: jwt 설치 및 사인 함수 생성

---
+++ src/lib/jwt-token.ts
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+const SECRET_KEY = process.env.JWT_SECRET_KEY || "your-secret-key";
+
+export const generateToken = (user) => {
+  return jwt.sign({ id: user.id, email: user.email }, SECRET_KEY, {
+    expiresIn: "1y", // 토큰 유효 기간 설정
+  });
+};
