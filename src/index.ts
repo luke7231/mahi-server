d2cebcc45886a0c83183851f3ae390e0ea93603e
luke7231 <tmddnjs7231@gmail.com>
2024-09-13 15:40:16 +0900

    test 주석 제거

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-// npm install @apollo/server express graphql cors
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
