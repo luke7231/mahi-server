32a6de09075758287fbc0820b29cdac364e89bc9
luke7231 <tmddnjs7231@gmail.com>
2024-09-16 17:09:55 +0900

    feat: nice 결제 핸들링 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import { resolvers } from "./resolvers/index.js";
 import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 import bodyParser from "body-parser";
+import { AuthResponse } from "./types.js";
+import axios from "axios";
 
 interface MyContext {
   token?: string;
@@ -35,29 +37,144 @@ const server = new ApolloServer<MyContext>({
   introspection: true,
 });
 // Ensure we wait for our server to start
-await server.start().then((res) => {
-  // app.use(bodyParser.json()); // JSON 형식의 본문 파싱
-  // app.use(bodyParser.urlencoded({ extended: true })); // URL-encoded 형식의 본문 파싱
+await server.start().then(async (res) => {
+  // const order = await prisma.order.findUnique()
+  app.use(bodyParser.json()); // JSON 형식의 본문 파싱
+  app.use(bodyParser.urlencoded({ extended: true })); // URL-encoded 형식의 본문 파싱
   // /nice-auth 엔드포인트 설정
-  // app.post("/nice-auth", (req, res) => {
-  //   const paymentResult = req.body;
-  //   console.log("결제 결과:", paymentResult); // 결제 결과 로그 출력
-  //   // const isSuccess = paymentResult.PCD_PAY_CODE === "0000";
-  //   const message = paymentResult.authResultMsg;
-  //   // const transactionId = paymentResult.transactionId || "unknown";
-  //   // 결제 결과를 쿼리 문자열로 포함하여 리디렉션
-  //   res.redirect(`${process.env.NICE_AUTH_REDIRECT_URL}?status=${message}`);
-  // });
-  // app.post("/payple-auth", (req, res) => {
-  //   const paymentResult = req.body;
-  //   console.log("결제 결과:", paymentResult); // 결제 결과 로그 출력
-  //   // 클라이언트 측의 특정 URL로 리디렉션
-  //   // const isSuccess = paymentResult.PCD_PAY_CODE === "0000";
-  //   const message = paymentResult.PCD_PAY_MSG;
-  //   // const transactionId = paymentResult.transactionId || "unknown";
-  //   // 결제 결과를 쿼리 문자열로 포함하여 리디렉션
-  //   res.redirect(`${process.env.NICE_AUTH_REDIRECT_URL}/nice-result?status=${message}`);
-  // });
+  app.post("/nice-auth", async (req, res) => {
+    // throw new Error("그냥 에러요~");
+    const paymentResult = req.body as AuthResponse;
+    const paymentResultAmount = paymentResult.amount;
+    console.log("auth 결과:", paymentResult); // 결제 결과 로그 출력
+    const cartItems = JSON.parse(paymentResult.mallReserved);
+    const isSuccess = paymentResult.authResultCode === "0000";
+    if (!isSuccess) {
+      console.log("결제에 실패했습니다.");
+    }
+    let ok: string;
+    let resCode: string;
+    const tid = paymentResult.tid;
+    const orderId = paymentResult.orderId;
+    const order = await prisma.order.findUnique({
+      where: { orderId },
+      include: { products: true },
+    });
+
+    if (!order) {
+      console.log("에러: order");
+    }
+    const isSame = order.amount === Number(paymentResultAmount);
+    if (!isSame) {
+      console.log("에러: isSame");
+    }
+
+    // *********************************
+    // 개수 남아있는지 마지막으로 파악
+    // *********************************
+    let canGo = true;
+    // 마지막으로 수량 체크해야함. 누군가 db 변경했을 수도 있잖.
+    for (const cartItem of cartItems) {
+      const existproduct = await prisma.product.findUnique({
+        where: { id: cartItem.product.id },
+      });
+      if (existproduct.isSoldout) {
+        const message = `${existproduct.name} 상품이 품절되었습니다.`;
+        res.redirect(
+          `${
+            process.env.NICE_AUTH_REDIRECT_URL
+          }?ok=${0}&code=${400}&message=${message}`
+        );
+        canGo = false;
+        return; // 중요: 여기서 return을 사용하여 함수를 종료
+      }
+      if (existproduct.quantity < cartItem.quantity) {
+        const message = `구매하려는 상품의 개수가 부족합니다. (남은 재고 ${existproduct.quantity}개)`;
+        res.redirect(
+          `${
+            process.env.NICE_AUTH_REDIRECT_URL
+          }?ok=${0}&code=${400}&message=${message}`
+        );
+        canGo = false;
+        return;
+      }
+    }
+
+    if (!canGo) return;
+    // *********************************
+    // ****** 승인 API 호출 시작 ******
+    // *********************************
+
+    const clientKey = "S2_7edb63a062cd4f799d14caa983faab78";
+    const secretKey = "08b2d64dad344d1bae91f443d7c981af";
+
+    // Step 2: Combine clientKey and secretKey with a colon separator
+    const credentials = `${clientKey}:${secretKey}`;
+
+    // Step 3: Base64 encode the combined string
+    const encodedCredentials = Buffer.from(credentials).toString("base64");
+
+    // Prepare the URL and headers
+    const url = `https://sandbox-api.nicepay.co.kr/v1/payments/${tid}`;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${encodedCredentials}`,
+    };
+
+    // Prepare the request data
+    const data = {
+      amount: Number(paymentResultAmount),
+    };
+
+    // Make the POST request using Axios
+    axios
+      .post(url, data, { headers })
+      .then(async (response) => {
+        // success
+        if (response.data.resultCode === "0000") {
+          await prisma.order.update({
+            where: { orderId },
+            data: { isApproved: true },
+          });
+
+          cartItems.map(async (cartItem) => {
+            const existproduct = await prisma.product.findUnique({
+              where: { id: cartItem.product.id },
+            });
+            const left = existproduct.quantity - cartItem.quantity;
+            const isSoldout = left <= 0;
+
+            await prisma.product.update({
+              where: { id: existproduct.id },
+              data: {
+                quantity: left,
+                isSoldout,
+              },
+            });
+          });
+          const amount = response.data.amount;
+          console.log("complete");
+          res.redirect(
+            `${
+              process.env.NICE_AUTH_REDIRECT_URL
+            }?ok=${1}&code=${200}&amount=${amount}`
+          );
+          return;
+        } else {
+          ok = "0";
+          resCode = "500";
+        }
+      })
+      .catch((error) => {
+        ok = "0";
+        resCode = "500";
+        res.redirect(
+          `${process.env.NICE_AUTH_REDIRECT_URL}?ok=${ok}&code=${resCode}`
+        );
+        return;
+      });
+    // 결제 결과를 쿼리 문자열로 포함하여 리디렉션
+  });
 });
 
 // Set up our Express middleware to handle CORS, body parsing,
